94f01cc14e753ccebcc665dd7f387667ec8502b5
Chuyen Tran <trantrungchuyen@humg.edu.vn>
2019-09-18 14:12:00 +0700

    Cách sử dụng class và thêm vào CSDL

--- EntityFrameworkDemo/Forms/MainForm.cs
+++ EntityFrameworkDemo/Forms/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityFrameworkDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,33 @@ namespace EntityFrameworkDemo
     public MainForm()
     {
       InitializeComponent();
+
+      // Tạo cơ sở dữ liệu nếu chưa có
+      DataAccess.DataAccess.CreateDatabase();
+
+      // Xóa cơ sở dữ liệu (nếu cần thiết)
+      //DataAccess.DataAccess.DeleteDatabase();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+      // Thử tạo một đối tượng Group (nhở using EntityFrameworkDemo.Models;)
+      Group group = new Group("ABC01");
+
+      // Thêm đối tượng vào CSDL
+      //DataAccess.DataAccess.AddGroup(group);
+
+      // Thử tạo một đối tượng Student
+      Student student = new Student("Nguyễn Văn A");
+      student.Score1 = 8;
+      student.Score2 = 6;
+      student.Score3 = 10;
+      MessageBox.Show("Điểm hệ 10: " + student.Grade().ToString()+ "; Điểm chữ: " + student.GradeA());
+      // Gán sinh viên thuộc nhóm học phần
+      student.Group = group;
+
+      // Thêm đối tượng sinh viên vào CSDL
+      //DataAccess.DataAccess.AddStudent(student);
     }
   }
 }
